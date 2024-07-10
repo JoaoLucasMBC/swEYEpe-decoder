9a1058a9f514d29fdedecc03d41d6b438cd3aa03
Joao Lucas <joaolucasmbcadorniga@gmail.com>
2024-07-10 15:09:52 -0500

    FEAT: timing all the points on main.py

--- src/main.py
+++ src/main.py
@@ -1,25 +1,30 @@
-from trie import Node, insert_key, print_trie
+from trie import Node, insert_key, print_trie, search_trie
 from keyboard import create_keyboard
 from predict import predict
 
 import pandas as pd
 
 def main():
-    with open('../data/eye-test.txt', 'r') as f:
+    with open('../data/eye-test4.txt', 'r') as f:
         lines: list[str] = f.readlines()
 
     df_training = pd.read_excel('../data/wordFrequency.xlsx', sheet_name='4 forms (219k)')
-    training_words = df_training['word'].tolist()
+
+    training_words = df_training['word'].tolist() #+ df_vocab['word'].tolist()
 
     # Filter only the words that are alpha
     training_words = [str(word).lower() for word in training_words if str(word).isalpha()]
     
-    keyboard = create_keyboard('../data/keyboard.txt')
+    keyboard = create_keyboard('../data/keyboard2.txt')
 
     # for each word, you have a list of tuples with all the coordinates of the eye while typing that word
     words = {}
     curr = ''
 
+    dt = 0.0166666667 # 60 fps
+    time = 0
+
+    # Parse the file and create the dictionary of words with the list of coordinates and the time for each point
     for line in lines:
         line = line.replace('\n', '')
         if line:
@@ -29,7 +34,8 @@ def main():
                 words[word] = []
             else:
                 x, y = map(float, line.split(','))
-                words[curr].append((x, y))
+                words[curr].append((x, y, time))
+                time += dt
     
     # Create the trie
     root = Node()
@@ -40,6 +46,6 @@ def main():
     for word in words:
         predict(word, words[word], keyboard, root)
 
-
+    
 if __name__ == "__main__":
     main()

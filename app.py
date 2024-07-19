2ca580fea56e2c2937967c39fd80f4e59aad5ecf
Joao Lucas <joaolucasmbcadorniga@gmail.com>
2024-07-19 15:49:50 -0500

    FEAT: changing app and requirements

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+
+from trie.keyboard import create_keyboard
+from trie.trie import Node, insert_key
+from trie.predict import predict
+
+app = Flask(__name__)
+
+
+keyboard = create_keyboard('../data/keyboard2.txt')
+
+df_training = pd.read_excel('../data/wordFrequency.xlsx', sheet_name='4 forms (219k)')
+
+training_words = df_training['word'].tolist() #+ df_vocab['word'].tolist()
+
+# Filter only the words that are alpha
+training_words = [str(word).lower() for word in training_words if str(word).isalpha()]
+
+# Create the trie
+root = Node()
+
+for word in training_words:
+    insert_key(root, word)
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict_word():
+    data = request.json
+
+    points = data['gaze_points']
+
+    dt = 0.0166666667 # 60 fps
+    time = 0
+
+    for i in range(len(points)):
+        x, y = points[i].values()
+        points[i] = (x, y, time)
+        time += dt
+
+
+    result = predict(points, keyboard, root)
+
+    print(result)
+
+    return jsonify({'top_words': result})
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
